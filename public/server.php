1b669ae43aed5016732363b87b110ac488fb5fa4
lnn <996475104@qq.com>
2019-11-21 15:32:16 +0800

    1

--- public/server.php
+++ public/server.php
@@ -1,14 +1,21 @@
 <?php
 //创建Server对象，监听 127.0.0.1:9501端口
-$serv = new Swoole\Server("127.0.0.1", 9501);
+$serv = new swoole_server("127.0.0.1", 9501);
 
+$serv->set(
+	[
+		'worker_num'=>8,//worker进程数
+		'max_request'=>10000,
+	]
+);
 //监听连接进入事件
-$serv->on('Connect', function ($serv, $fd) {
-	echo "Client: Connect.\n";
+//$fd 客户端连接的唯一标识  $reactor_id  线程id
+$serv->on('Connect', function ($serv, $fd,$reactor_id) {
+	echo "Client: {$fd} - {$reactor_id}.\n";
 });
 
 //监听数据接收事件
-$serv->on('Receive', function ($serv, $fd, $from_id, $data) {
+$serv->on('Receive', function ($serv, $fd, $reactor_id, $data) {
 	$serv->send($fd, "Server: ".$data);
 });
 
