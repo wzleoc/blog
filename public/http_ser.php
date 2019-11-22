081655d2034f59dac45b194e66a3f6a487953c8e
lnn <996475104@qq.com>
2019-11-22 15:26:23 +0800

    1

---
+++ public/http_ser.php
@@ -0,0 +1,8 @@
+<?php
+$http = new Swoole\Http\Server('0.0.0.0','8811');
+$http->on('request',function($request,$response){
+
+	$aa = $request->get;
+//	print_r($aa);
+	$response->end('rrrrrrr'.json_encode($aa));
+});
