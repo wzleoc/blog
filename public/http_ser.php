5d246b72eeaa4ce1bdff250758c2bf6de97fc41b
lnn <996475104@qq.com>
2019-11-25 15:45:44 +0800

    1

--- public/http_ser.php
+++ public/http_ser.php
@@ -1,6 +1,7 @@
 <?php
-$http = new Swoole\Http\Server("0.0.0.0", 8811);
-$http->on('request', function ($request, $response) {
-	$response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
-});
-$http->start();
+//$http = new Swoole\Http\Server("0.0.0.0", 8811);
+//$http->on('request', function ($request, $response) {
+//	$response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+//});
+//$http->start();
+echo 11111111;
