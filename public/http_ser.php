66aaabf11a603eaa890580fa244d5f25ec73a17d
lnn <996475104@qq.com>
2019-11-25 15:42:24 +0800

    111

--- public/http_ser.php
+++ public/http_ser.php
@@ -1,9 +1,6 @@
 <?php
-$http = new Swoole\Http\Server('0.0.0.0','8811');
-$http->on('request',function($request,$response){
-
-//	$aa = $request->get;
-//	print_r($aa);
-	$response->end('rrrrrrr');
+$http = new Swoole\Http\Server("0.0.0.0", 8811);
+$http->on('request', function ($request, $response) {
+	$response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
 });
 $http->start();
