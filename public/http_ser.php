f4caa87a639cf6b1167bd50eb8706c9f1fecff4f
lnn <996475104@qq.com>
2019-11-25 16:37:45 +0800

    1

--- public/http_ser.php
+++ public/http_ser.php
@@ -1,7 +1,11 @@
 <?php
 $http = new Swoole\Http\Server("0.0.0.0", 8811);
+$http->set([
+	'enable_static_handler'=>true,
+	'document_root'=>'./'
+]);
 $http->on('request', function ($request, $response) {
-	$request->cookie('lnnscookie','hahaha',time()+1800);
+
 	$response->end(111111111);
 });
 $http->start();
